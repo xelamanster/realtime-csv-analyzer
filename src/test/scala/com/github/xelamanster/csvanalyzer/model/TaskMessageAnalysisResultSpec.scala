28877907dc94378f59451222f144eaa33b557c07
Oleksandr Chugunov <xelamanster@gmail.com>
2019-06-22 13:26:07 +0300

    Added tests for TaskMessageAnalysisResult encoder.

---
+++ src/test/scala/com/github/xelamanster/csvanalyzer/model/TaskMessageAnalysisResultSpec.scala
@@ -0,0 +1,38 @@
+package com.github.xelamanster.csvanalyzer.model
+
+import java.util.UUID
+
+import akka.util.ByteString
+import com.github.xelamanster.csvanalyzer.utils.csv.Format.syntax._
+import com.github.xelamanster.csvanalyzer.model.TaskMessageAnalysisResult.implicits._
+import org.scalatest._
+
+class TaskMessageAnalysisResultSpec extends WordSpec with Matchers {
+
+  "TaskMessageAnalysisResultEncoder" should {
+
+    "encode analysis result" in {
+      val analysisResult =
+        TaskMessageAnalysisResult(
+          24425,
+          1,
+          Seq(
+            UserAnalysisResult(
+              UserId(UUID.fromString("b0c09889-1899-4b41-80cf-d01b32ab3222")),
+              2.34,
+              24425),
+          )
+        )
+
+      val expectedEncodedStringValue =
+        """24425
+          |1
+          |b0c09889-1899-4b41-80cf-d01b32ab3222,2.34,24425""".stripMargin
+
+      analysisResult.encodeString() should be(expectedEncodedStringValue)
+      analysisResult.encode() should be(ByteString(expectedEncodedStringValue))
+    }
+
+  }
+
+}
